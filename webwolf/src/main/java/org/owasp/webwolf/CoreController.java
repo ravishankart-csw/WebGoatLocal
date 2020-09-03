e921f2b0bf146fd0dca95227ce801defb13fbd7b
kiran <kiran@Manus-iMac.local>
2020-09-03 18:08:42 +0530

    Added CoreController.java

---
+++ webwolf/src/main/java/org/owasp/webwolf/CoreController.java
@@ -0,0 +1,16 @@
+package org.owasp.webwolf;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController
+public class CoreController {
+
+    @RequestMapping(value = "/talk")
+    public String talk(String talkee) {
+        return "Hi";
+    }
+}
